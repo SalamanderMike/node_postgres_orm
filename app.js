afcdec15a808f43eebe2cefca6b11e18baee649e
salamandermike <salamandermike@gmail.com>
2014-07-27 22:27:47 -0700

    Complete

--- app.js
+++ app.js
@@ -1,49 +1,98 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
+  pg = require('pg'),
   Person = require('./models/main.js').Person,
   app = express();
 
-
-
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public')); // get static file: CSS
 // Middleware
 app.use(bodyParser.urlencoded());
 app.use(methodOverride("_method"));
 
+app.get('/', function (req, res) {
+  res.redirect('/people');
+});
 
+// **** BONUS: Static Constructor under construction *****
+var Record = function(params){
+  this.params = params;
 
+  this.findMe = Person.findBy(this.params, function (err, foundPerson){
+    if (foundPerson) {
+      res.render("people/show", {people: foundPerson});
+    } else {
+      res.render("people/error");
+    };
+  });
+}
 
+// Site Pages
 app.get("/people", function(req, res){
-  res.render("people/index", {people: []})
+  Person.all(function (err, allLovelyPeople){ // Person.all(this is the callback)
+    res.render("people/index", {people: allLovelyPeople}) // people = allLovelyPeople
+  });
+});
+
+app.get("/people/find", function(req, res){
+  res.render('people/find');
 });
 
 app.get("/people/new", function(req, res){
-  res.render("people/new")
-});
-
-app.get("/people/:id", function(req,res){
-  res.render("people/show", {person: {} });
-});
-
-app.get("/people/:id/edit", function(req,res){
-  res.render("people/edit", {person: {} });
+  res.render("people/new");
 });
 
 
 
-app.post("/people", function(req, res){
-  res.redirect("/people")
+// Calc Engine: FIND person
+app.post("/people/find", function(req, res){
+  var val = req.body.person.lastname;
+  Person.findBy(val, function (err, foundPerson){
+    if (foundPerson) {
+      res.render("people/show", {people: foundPerson});
+    } else {
+      res.render("people/error");
+    };
+  });
 });
 
-app.delete("/people/:id", function(req, res){
-  res.redirect("/people");
+// // Constructor FIND ***** Under Construction *****
+// app.post("/people/find", function(req, res){
+//   var val = req.body.person.lastname;
+//   var findPerson = new Record(val);
+
+//   findPerson.findMe();
+// });
+
+
+// Calc Engine: NEW person
+app.post("/people/new", function(req, res){
+  var params = req.body.person;
+  Person.create(params, function (err){
+    res.redirect("/people");
+  });
 });
 
-app.put("/people/:id", function(req,res){
-  res.redirect("/people");
-})
+// DELETE
+app.delete("/people/delete", function(req, res){
+  var id = req.body.person.id;
+  Person.prototype.destroy(id, function (err){
+    res.redirect("/people");
+  });
+});
+
+// UPDATE
+app.put("/people/update", function(req,res){
+  var params = req.body.person;
+  Person.prototype.update(params, function (err){
+    res.redirect("/people");
+  });
+});
+
+
+
 
 app.listen(3000, function(){
-  console.log("THE SERVER IS LISTENING ON localhost:3000");
+  console.log("NODEMON RIDING IN THE CODE-VAN localhost:3000");
 });
--- models/person.js
+++ models/person.js
@@ -7,67 +7,77 @@ function Person(params) {
 };
 
 
-Person.all = function(callback){
-  db.query("YOUR QUERY HERE",[], function(err, res){
-    var allPeople = [];
-    // do something here with res
-    callback(err, allPeople);
-  });
-}
+Person.all = function(callback){ // callback = function from app.js
+  db.query("SELECT * FROM people;",[], function(err, res){
+    var allTheLonelyPeople = [];// Create an array
+    if (!err){// if no error, do...
+      res.rows.forEach(function(individual){        // Read each row and do...
+        allTheLonelyPeople.push(new Person(individual));// array.push(the Person)
+      });
+    } else {
+      console.log('Ack!!!');
+    };
+    callback(err, allTheLonelyPeople);// Now, do function from app.js
+  });                              // and pass back the value of 
+};                                 // allTheLonelyPeople
 
-Person.findBy = function(key, val, callback) {
-  db.query("",[val], function(err, res){
-    var foundRow, foundPerson;
-    // do something here with res
-    callback(err, foundPerson);
-  });
-};
-
-
+Person.findBy = function(surname, callback) {// callback = function from app.js
+  db.query("SELECT * FROM people WHERE lastname = $1;",[surname], function(err, res){
+    if (res.rows[0] !== undefined){
+      foundPerson = [res.rows[0]];
+    } else {
+      foundPerson = false;
+    }
+    callback(err, foundPerson);// Now, do function from app.js
+  });                              // and pass back the value of 
+};                                 // foundPerson
 
 Person.create = function(params, callback){
-  db.query("", [params.firstname, params.lastname], function(err, res){
-    var createdRow, newPerson;
-    callback(err, newPerson);
+  db.query("INSERT INTO people (firstname, lastname) VALUES ($1, $2) RETURNING *", [params.firstname, params.lastname], function(err, res){
+    callback();
   });
 };
 
 Person.prototype.update = function(params, callback) {
+  console.log(params.id);
   var colNames = [];
   var colVals = [];
-  var count = 2;
+  var count = 2; // start at firstname... $2...
 
-  for(var key in this) {
-    if(this.hasOwnProperty(key) && params[key] !== undefined){
+  for(var key in params) {// ...creating statement of key:value pairs for SQL
+    if(params.hasOwnProperty(key) && params[key] !== undefined){
       var colName = key + "=$" + count;
       colNames.push(colName);
       colVals.push(params[key]);
       count++;
-    }
+    };
   }
-
+// Putting the statement together: UPDATE... id, firstname, lastname...
   var statement = "UPDATE people SET " + colNames.join(", ") + " WHERE id=$1 RETURNING *";
-  var values = [this.id].concat(colVals);
+  var values = [params.id].concat(colVals);// [1,Bob,Brown,1]
+
   console.log("Running:");
   console.log(statement, "with values", values);
-  var _this = this;
+  var _this = params;
+
+// Calling the statement
   db.query(statement, values, function(err, res) {
     var updatedRow;
-    if(err) {
-      console.error("OOP! Something went wrong!", err);
-    } else {
+    if(!err) {
       updatedRow = res.rows[0];
       _this.firstname = updatedRow.firstname;
       _this.lastname = updatedRow.lastname;
+    } else {
+      console.error("OOP! Something went wrong!", err);
     }
     callback(err, _this)
   });
-}
+};
 
-Person.prototype.destroy = function(){
-  db.query("", [this.id], function(err, res) {
+Person.prototype.destroy = function(id, callback){
+  db.query("DELETE FROM people WHERE id = $1", [id], function(err, res) {
     callback(err)
   });
-}
+};
 
 module.exports = Person;
